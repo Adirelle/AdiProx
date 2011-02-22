a5e9b3c6148d2b9dd02486aa1e5a5135f8770dfe
Adirelle <adirelle@tagada-team.net>
2011-02-22 22:47:57 +0100

    Added a test encounter module.

---
+++ modules/encounters/Test.lua
@@ -0,0 +1,48 @@
+--[[
+AdiProx - Proximity display.
+Copyright 2011 Adirelle (adirelle@tagada-team.net)
+All rights reserved.
+--]]
+
+local addonName, addon = ...
+local L = addon.L
+
+local LibMapData = LibStub('LibMapData-1.0')
+
+local mod = addon:NewEncounterModule('Test')
+mod.maps = { "Orgrimmar" }
+
+function mod:OnEnable()
+	self:RegisterCombatLogEvent('SPELL_AURA_APPLIED')
+	self:RegisterCombatLogEvent('SPELL_AURA_REMOVED')
+	self:RegisterCombatLogEvent('SPELL_CAST_SUCCESS')
+end
+
+function mod:SPELL_AURA_APPLIED(_, event)
+	if event.spellId == 33763 then
+		local pos = addon:GetUnitPosition(event.destGUID)
+		if pos and not pos:GetWidget("lifebloom") then
+			pos:Attach("lifebloom", self:AcquireWidget("range", [[SPELLS\CIRCLE]], 5, 1, 0, 1, 0))
+		end
+	end
+end
+
+function mod:SPELL_AURA_REMOVED(_, event)
+	if event.spellId == 33763 then
+		local pos = addon:GetUnitPosition(event.destGUID)
+		local widget = pos and pos:GetWidget("lifebloom")
+		if widget then
+			widget:Release()
+		end
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(_, event)
+	if event.spellId == 18562 then
+		local pos = addon:GetUnitPosition(event.destGUID)
+		if pos and not pos:GetWidget("swiftmend") then
+			pos:Attach("swiftmend", self:AcquireWidget("range", [[SPELLS\WHITERINGTHIN128]], 10, 1, 0, 1, 0, 1, "ADD")):SetDuration(7)
+		end
+	end
+end
+
