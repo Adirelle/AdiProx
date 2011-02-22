e0df33cec894510d20fec351a1ecbdb18abcf41c
Adirelle <adirelle@tagada-team.net>
2011-02-22 22:47:48 +0100

    Enhanced the CLEU dispatcher.

--- modules/Encounters.lua
+++ modules/Encounters.lua
@@ -20,7 +20,10 @@ end
 
 function mod:PostDisable()
 	LibMapData.UnregisterAllCallbacks(self)
-	wipe(self.eventRegistry)
+end
+
+function addon:NewEncounterModule(...)
+	return mod:NewModule(...)
 end
 
 local bossIDs = {}
@@ -38,7 +41,7 @@ function mod:UpdateModules()
 	end
 	
 	-- Check each module
-	for _, module in self:IterateModules()		
+	for _, module in self:IterateModules()do	
 		local enable = false
 		
 		-- Check map-related modules
@@ -82,6 +85,7 @@ end
 
 mod:SetDefaultModulePrototype(moduleProto)
 mod:SetDefaultModuleState(false)
+mod:SetDefaultModuleLibraries('AceEvent-3.0')
 
 function moduleProto:OnDisable()
 	addon.moduleProto.OnDisable(self)
@@ -92,26 +96,104 @@ end
 -- Combat listening
 --------------------------------------------------------------------------------
 
-mod.callbacks = LibStub('CallbackHandler-1.0'):New(moduleProto, "RegisterCombatLogEvent", "UnregisterCombatLogEvent", "UnregisterAllCombatLogEvents")
+local callbacks = LibStub('CallbackHandler-1.0'):New(moduleProto, "RegisterCombatLogEvent", "UnregisterCombatLogEvent", "UnregisterAllCombatLogEvents")
 
-mod.eventRegistry = {}
+local eventRegistry = {}
 
-function mod.callbacks:OnUsed(_, event)
-	if not next(mod.eventRegistry) then
+function callbacks:OnUsed(_, event)
+	if not next(eventRegistry) then
+		mod:Debug('Registered COMBAT_LOG_EVENT_UNFILTERED')
 		mod:RegisterEvent('COMBAT_LOG_EVENT_UNFILTERED')
 	end
-	mod.eventRegistry[event] = true
+	eventRegistry[event] = true
 end
 
-function mod.callbacks:OnOnused(_, event)
-	mod.eventRegistry[event] = nil
-	if not next(mod.eventRegistry) then
+function callbacks:OnOnused(_, event)
+	eventRegistry[event] = nil
+	if not next(eventRegistry) then
 		mod:UnregisterEvent('COMBAT_LOG_EVENT_UNFILTERED')
+		mod:Debug('Unregistered COMBAT_LOG_EVENT_UNFILTERED')
 	end
 end
+
+local evt = {}
+local fillers = {}
 
 function mod:COMBAT_LOG_EVENT_UNFILTERED(_, _, event, ...)
-	if self.eventRegistry[event] then
-		mod.callbacks:Fire(event, ...)
+	if eventRegistry[event] then
+		callbacks:Fire(event, fillers[event](evt, event, ...))
 	end
 end
+
+-- Dynamically build functions to fill the event table
+do
+	local prefixes = {
+		SWING = "",
+		RANGE = ",spellId,spellName,spellSchool",
+		SPELL = ",spellId,spellName,spellSchool",
+		SPELL_PERIODIC = ",spellId,spellName,spellSchool",
+		SPELL_BUILDING = ",spellId,spellName,spellSchool",
+		ENVIRONMENTAL = ",environmentalType",
+	}
+	local suffixes = {
+		DAMAGE = ",amount,overkill,school,resisted,blocked,absorbed,critical,glancing,crushing",
+		MISSED = ",missType,amountMissed",
+		HEAL = ",amount,overhealing,absorbed,critical",
+		ENERGIZE = ",amount,powerType",
+		DRAIN = ",amount,powerType,extraAmount",
+		LEECH = ",amount,powerType,extraAmount",
+		INTERRUPT = ",extraSpellID,extraSpellName,extraSchool",
+		DISPEL = ",extraSpellID,extraSpellName,extraSchool,auraType",
+		DISPEL_FAILED = ",extraSpellID,extraSpellName,extraSchool",
+		STOLEN = ",extraSpellID,extraSpellName,extraSchool,auraType",
+		EXTRA_ATTACKS = ",amount",
+		AURA_APPLIED = ",auraType",
+		AURA_REMOVED= ",auraType",
+		AURA_APPLIED_DOSE = ",auraType,amount",
+		AURA_REMOVED_DOSE = ",auraType,amount",
+		AURA_REFRESH = ",auraType",
+		AURA_BROKEN = ",auraType",
+		AURA_BROKEN_SPELL = ",extraSpellID,extraSpellName,extraSchool,auraType",
+		CAST_START = "",
+		CAST_SUCCESS = "",
+		CAST_FAILED	= ",failedType",
+		INSTAKILL = "",
+		DURABILITY_DAMAGE = "",
+		DURABILITY_DAMAGE_ALL = "",
+		CREATE = "",
+		SUMMON = "",
+		RESURRECT = "",
+	}
+
+	-- Build filler functions based on the argument list
+	local fillerByArgs = setmetatable({}, {__index = function(self, args)
+		local body = format("return function(e,%s) wipe(e) e.%s = %s return e end", args, gsub(args, ",", ",e."), args)
+		local result = loadstring(body, args)()
+		self[args] = result
+		return result
+	end})
+	
+	-- Build filler by event
+	setmetatable(fillers, {__index = function(self, event)
+		local args = "event,sourceGUID,sourceName,sourceFlags,destGUID,destName,destFlags"
+		for prefix, prefixArgs in pairs(prefixes) do
+			local len = strlen(prefix)
+			if substr(event, 1, len) == prefix then
+				local suffixArgs = suffixes[substr(event, len + 2)]
+				if suffixArgs then
+					args = args..prefixArgs..suffixArgs
+					break
+				end
+			end
+		end
+		local filler = fillerByArgs[args]
+		self[event] = filler
+		return filler
+	end})
+
+	-- Enforced aliases
+	fillers.DAMAGE_SHIELD = fillers.SPELL_DAMAGE
+	fillers.DAMAGE_SPLIT = fillers.SPELL_DAMAGE
+	fillers.DAMAGE_SHIELD_MISSED = fillers.SPELL_MISSED
+end
+
