84c469d5557dd8267603ea0c5e03a7cbfc9fa46d
Adirelle <adirelle@tagada-team.net>
2011-02-25 11:28:59 +0100

    Added spell cast support to encounter module prototype.

--- modules/Encounters.lua
+++ modules/Encounters.lua
@@ -100,26 +100,22 @@ end
 
 mod:SetDefaultModulePrototype(moduleProto)
 mod:SetDefaultModuleState(false)
-mod:SetDefaultModuleLibraries('LibCombatLogEvent-1.0')
+mod:SetDefaultModuleLibraries('LibCombatLogEvent-1.0', 'AceEvent-3.0', 'AceTimer-3.0')
 
 function moduleProto:OnEnable()
 	if self.auras then
 		self:RegisterCombatLobEvent('SPELL_AURA_APPLIED', 'OnAuraApplied')
+		self:RegisterCombatLobEvent('SPELL_AURA_APPLIED_DOSE', 'OnAuraApplied')
 		self:RegisterCombatLobEvent('SPELL_AURA_REFRESH', 'OnAuraApplied')
 		self:RegisterCombatLobEvent('SPELL_AURA_REMOVED', 'OnAuraRemoved')
 	end
+	if self.spellCasts then
+		self.currentCast = wipe(self.currentCast or {})
+		self:RegisterCombatLobEvent('SPELL_CAST_START')
+		self:RegisterCombatLobEvent('SPELL_CAST_SUCCESS')
+	end
 	if self.PostEnable then
 		self:PostEnable()
-	end
-end
-
-function moduleProto:GetMobUnit(mobID)
-	if mobUnits[mobID] then
-		return mobUnits[mobID]
-	elseif GetMobID(UnitGUID("target")) == mobID then
-		return "target"
-	elseif GetMobID(UnitGUID("focus")) == mobID then
-		return "focus"
 	end
 end
 
@@ -132,9 +128,8 @@ function moduleProto:OnAuraApplied(event, args)
 			local widget = position:GetWidget(key)
 			if not widget then
 				-- DO SOMETHING
-			elseif event == "SPELL_AURA_REFRESH" then
-				-- DO SOMETHING
 			end
+			-- DO SOMETHING
 			if self.PostAuraApplied then
 				self:PostAuraApplied(event, args, position, widget)
 			end
@@ -154,3 +149,75 @@ function moduleProto:OnAuraRemoved(event, args)
 	end
 end
 
+-- Quite ugly, but quick
+local UnitGUID, GetNumRaidMembers, GetNumPartyMembers = UnitGUID, GetNumRaidMembers, GetNumPartyMembers
+function moduleProto:MobGUIDToUnit(guid)
+	if not guid then return
+	elseif UnitGUID("boss1") == guid then return "boss1"
+	elseif UnitGUID("boss2") == guid then return "boss2"
+	elseif UnitGUID("boss3") == guid then return "boss3"
+	elseif UnitGUID("boss4") == guid then return "boss4"
+	elseif UnitGUID("target") == guid then return "target"
+	elseif UnitGUID("focus") == guid then return "focus"
+	elseif GetNumRaidMembers() > 0 then
+		for i = 1, GetNumRaidMembers() do
+			if UnitGUID("raidtarget"..i) == guid then return "raidtarget"..i end
+		end
+	elseif GetNumPartyMembers() > 0 then
+		for i = 1, GetNumPartyMembers() do
+			if UnitGUID("partytarget"..i) == guid then return "partytarget"..i end
+		end
+	end
+end
+
+function moduleProto:GetSpellCastTarget(args)
+	local unit = self:MobGUIDToUnit(args.sourceGUID)
+	if not unit then return end
+	local target = gsub(unit.."target", "(%d+)target$", "target%1")	
+	args.destName = UnitName(target)
+	args.destGUID = UnitGUID(target)
+	return self:OnSpellCast(args.event, args)
+end
+
+function moduleProto:SPELL_CAST_START(event, args)
+	if self.spellCasts[args.spellId] then
+		wipe(self.currentCast)
+		for k, v in pairs(args) do
+			self.currentCast[k] = v
+		end
+		self:ScheduleTimer("GetSpellCastTarget", 0.1, self.currentCast)
+	end	
+end
+
+function moduleProto:SPELL_CAST_SUCCESS(event, args)
+	if self.spellCasts[args.spellId]  then
+		return self:OnSpellCast(event, args)
+	end
+end
+
+function moduleProto:OnSpellCast(event, args)
+	local spell = self.spellCasts[args.spellId] 
+	if spell then
+		local position = addon:GetUnitPosition(arg.destGUID)
+		if position then
+			local key = "spell"..args.spellId
+			local widget = position:GetWidget(key)
+			if not widget then
+				-- DO SOMETHING
+			end
+			if self.PostSpellCast then
+				self:PostSpellCast(event, args, position, widget)
+			end
+		end
+	end
+end
+
+--------------------------------------------------------------------------------
+-- Encounter widgets
+--------------------------------------------------------------------------------
+
+local reticleProto, parentProto = addon.NewWidgetType("reticle", "abstract")
+
+function reticleProto:OnCreateFrame(parent)
+	local frame = 
+end
