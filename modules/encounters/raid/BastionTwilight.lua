f76c544fe69a4d2a7766444fc449da5a1485cb85
Adirelle <adirelle@tagada-team.net>
2011-02-26 01:06:19 +0100

    Added some raid encounters.

---
+++ modules/encounters/raid/BastionTwilight.lua
@@ -0,0 +1,27 @@
+--[[
+AdiProx - Proximity display.
+Copyright 2011 Adirelle (adirelle@tagada-team.net)
+All rights reserved.
+--]]
+
+local addonName, addon = ...
+
+addon:NewEncounterModule('Bastion of Twilight - Maloriak')
+	:AgainstMobs(41378)
+	:WatchAuras(77699, 92978, 92979, 92980, { range = 5, duration = 30 }) -- Flash Freeze
+	:WatchAuras(77760, 92975, 92976, 92977, { range = 5 }) -- Biting Chill
+
+addon:NewEncounterModule('Bastion of Twilight - Magmaw')
+	:AgainstMobs(41570)
+	:WatchAuras(91913, 94678, 94679, { range = 5, duration = 10 }) -- Parasitic Infection
+
+addon:NewEncounterModule('Bastion of Twilight - Atramedes')
+	:AgainstMobs(41442)
+	:WatchAuras(78092) -- Tracking
+
+addon:NewEncounterModule('Bastion of Twilight - Omnotron Defense System')
+	:AgainstMobs(42180, 42178, 42179, 42166)
+	:WatchAuras(80094, { duration = 20 }) -- Fixate
+	:WatchAuras(79888, 91431, 91432, 91433, { range = 20, duration = 10 }) -- Lightning Conductor
+	:WatchAuras(79501, 92035, 92036, 92037, { duration = 4 }) -- Acquiring Target
+
---
+++ modules/encounters/raid/BlackwingDescent.lua
@@ -0,0 +1,15 @@
+--[[
+AdiProx - Proximity display.
+Copyright 2011 Adirelle (adirelle@tagada-team.net)
+All rights reserved.
+--]]
+
+local addonName, addon = ...
+
+addon:NewEncounterModule('Bastion of Twilight - Valiona & Theralion')
+	:AgainstMobs(45992, 45993)
+	:WatchAuras(86788, 92876, 92877, 92878, { range = -10, duration = 15 }) -- Blackout
+	:WatchAuras(92861, 86013, 92859, { range = 8, duration = 6 }) -- Twilight Meteorite
+	:WatchAuras(86622, 95639, 95640, 95641, { range = 15, duration = 20 }) -- Engulfing Magic
+	:WatchSpellCasts(92900, 86369, 92899, 92898, { range = -8, duration = 2 }) -- Twilight Blast
+
